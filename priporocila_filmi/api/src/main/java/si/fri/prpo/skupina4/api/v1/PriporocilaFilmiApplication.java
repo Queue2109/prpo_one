df261bff7235f0361b15c8e4c41fbfe3fae13af6
bucarjeva <jb2662@student.uni-lj.si>
2023-12-09 12:44:22 +0100

    packaged in v1, created Application class

---
+++ priporocila_filmi/api/src/main/java/si/fri/prpo/skupina4/api/v1/PriporocilaFilmiApplication.java
@@ -0,0 +1,8 @@
+package si.fri.prpo.skupina4.api.v1;
+
+import javax.ws.rs.ApplicationPath;
+import javax.ws.rs.core.Application;
+
+@ApplicationPath("v1")
+public class PriporocilaFilmiApplication extends Application {
+}
