a649d93f81f133c0829b890529d73ad25b8b2733
bucarjeva <jb2662@student.uni-lj.si>
2023-12-09 16:52:16 +0100

    dokoncan interceptor

--- priporocila_filmi/storitve/src/main/java/si/fri/prpo/skupina4/interceptorji/BelezenjeKlicevInterceptor.java
+++ priporocila_filmi/storitve/src/main/java/si/fri/prpo/skupina4/interceptorji/BelezenjeKlicevInterceptor.java
@@ -1,8 +1,26 @@
 package si.fri.prpo.skupina4.interceptorji;
 
+import javax.interceptor.AroundInvoke;
 import javax.interceptor.Interceptor;
+import javax.interceptor.InvocationContext;
+import java.util.logging.Logger;
 
 @Interceptor
 @BeleziKlice
 public class BelezenjeKlicevInterceptor {
+
+    private Logger log = Logger.getLogger(BelezenjeKlicevInterceptor.class.getName());
+
+    @AroundInvoke
+    public Object beleziKlice(InvocationContext context) throws Exception {
+        log.info("vstop v metodo: " + context.getClass().getName());
+        try{
+            Object result = context.proceed();
+            log.info("izstop iz metode: " + context.getClass().getName());
+            return result;
+        } catch (Exception e) {
+            log.info("izjema v metodi: " + context.getClass().getName());
+            throw e;
+        }
+    }
 }
