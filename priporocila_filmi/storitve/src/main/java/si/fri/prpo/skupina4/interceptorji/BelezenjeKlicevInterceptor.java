0ac4bd9f0df0d7d753727abaea571c91e54d092b
bucarjeva <jb2662@student.uni-lj.si>
2023-12-10 12:29:43 +0100

    belezenje st za vsako metodo posebej

--- priporocila_filmi/storitve/src/main/java/si/fri/prpo/skupina4/interceptorji/BelezenjeKlicevInterceptor.java
+++ priporocila_filmi/storitve/src/main/java/si/fri/prpo/skupina4/interceptorji/BelezenjeKlicevInterceptor.java
@@ -3,6 +3,7 @@ package si.fri.prpo.skupina4.interceptorji;
 import javax.interceptor.AroundInvoke;
 import javax.interceptor.Interceptor;
 import javax.interceptor.InvocationContext;
+import java.util.HashMap;
 import java.util.logging.Logger;
 
 @Interceptor
@@ -11,15 +12,23 @@ public class BelezenjeKlicevInterceptor {
 
     private Logger log = Logger.getLogger(BelezenjeKlicevInterceptor.class.getName());
 
+    static HashMap<String, Integer> klici = new HashMap<>();
+
     @AroundInvoke
     public Object beleziKlice(InvocationContext context) throws Exception {
-        log.info("vstop v metodo: " + context.getClass().getName());
+        String metoda = context.getMethod().toString();
+        log.info("vstop v metodo: " + metoda);
+
+        int st = klici.getOrDefault(metoda, 0) + 1;
+        klici.put(metoda, st);
+        log.info(Integer.toString(st));
+
         try{
             Object result = context.proceed();
-            log.info("izstop iz metode: " + context.getClass().getName());
+            log.info("izstop iz metode: " + metoda);
             return result;
         } catch (Exception e) {
-            log.info("izjema v metodi: " + context.getClass().getName());
+            log.info("izjema v metodi: " + metoda);
             throw e;
         }
     }
