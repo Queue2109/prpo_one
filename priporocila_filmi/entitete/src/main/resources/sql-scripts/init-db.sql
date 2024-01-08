ed7b98b3b8f6426fc4c5608deadbb40f606a4df1
Queue2109 <hj2109@student.uni-lj.si>
2024-01-08 19:45:11 +0100

    init-db update

--- priporocila_filmi/entitete/src/main/resources/sql-scripts/init-db.sql
+++ priporocila_filmi/entitete/src/main/resources/sql-scripts/init-db.sql
@@ -17,7 +17,7 @@ INSERT INTO film (naslov, opis, leto_izzida, zanr_id, ocena) VALUES ('Shrek 2', 
 INSERT INTO uporabnik (uporabnisko_ime, geslo, email, spol, starost) VALUES ('petrakos', 'geslo123', 'petra.kos@hotmail.com', 'Z', 45);
 INSERT INTO uporabnik (uporabnisko_ime, geslo, email, spol, starost) VALUES ('mihakovac', 'geslo456', 'miha.kovac@gmail.com', 'M', 23);
 
-INSERT INTO ocena (uporabnik_id, film_id, ocena, komentar, cas_objave) VALUES (1, 1, 10, 'Najboljsi film, kar sem ga kdajkoli gledal', '2023-10-10 19:08:57');
-INSERT INTO ocena (uporabnik_id, film_id, ocena, komentar, cas_objave) VALUES (1, 2, 8, 'Film je bil slabši od prvega, vendar še vseeno zelo smešen.', '2023-10-18 11:10:57');
+INSERT INTO ocena (uporabnik_id, film_id, ocena, komentar, cas_objave) VALUES (1, 1, 8, 'Najboljsi film, kar sem ga kdajkoli gledal', '2023-10-10 19:08:57');
+INSERT INTO ocena (uporabnik_id, film_id, ocena, komentar, cas_objave) VALUES (1, 2, 7, 'Film je bil slabši od prvega, vendar še vseeno zelo smešen.', '2023-10-18 11:10:57');
 
 INSERT INTO uporabnik_zanr (uporabnik_id, zanr_id) VALUES (1, 1);
